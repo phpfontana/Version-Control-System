f77aade556e18de0c43addfd45525c3c37200bb1
Pedro H. P. Fontana <104539928+phpfontana@users.noreply.github.com>
2023-06-25 23:26:47 -0300

    Add files via upload

---
+++ src/utils.h
@@ -0,0 +1,10 @@
+# ifndef __UTILS_H__
+# define __UTILS_H__
+
+char* hash();
+char* timestamp();
+void printInfo(const char* message);
+void printAlert(const char* message);
+void printWarning(const char* message);
+
+# endif
